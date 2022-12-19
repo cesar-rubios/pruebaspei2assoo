c3127147df8f64e242166d1c7fb7e7f0eade7294
César-Rubio-Sánchez <95412713+cesar-rubios@users.noreply.github.com>
2022-12-19 11:25:24 +0100

    Update main.py

--- mipropioapi/compose-yml/app/main.py
+++ mipropioapi/compose-yml/app/main.py
@@ -58,4 +58,4 @@ def catalog():
 
 if __name__ == '__main__':
     APP.debug = True
-    app.run(port=int(os.environ.get("PORT", 5000)),host='0.0.0.0',debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
